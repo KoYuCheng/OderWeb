db4935c22b8f34d9de325020afa388f0bd63c3fa
KoYuCheng <108333876+KoYuCheng@users.noreply.github.com>
2022-07-07 12:37:04 +0800

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,132 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Label">請輸入訂位資訊</asp:Label>
+    
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="姓名:"></asp:Label>
+    
+        <asp:TextBox ID="textname" runat="server" OnTextChanged="textname_TextChanged" ></asp:TextBox>
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Label">電話:</asp:Label>
+    
+        <asp:TextBox ID="texttel" runat="server" OnTextChanged="texttel_TextChanged" MaxLength="10" AutoPostBack="True" CausesValidation="True"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Label">訂位日期:</asp:Label>
+    
+        <asp:Calendar ID="Calendar1" runat="server" BackColor="White" BorderColor="#999999" CellPadding="4" DayNameFormat="Shortest" Font-Names="Verdana" Font-Size="8pt" ForeColor="Black" Height="245px" Width="280px" OnSelectionChanged="Calendar1_SelectionChanged" OnDayRender="Calendar1_DayRender">
+            <DayHeaderStyle BackColor="#CCCCCC" Font-Bold="True" Font-Size="7pt" />
+            <NextPrevStyle VerticalAlign="Bottom" />
+            <OtherMonthDayStyle ForeColor="#808080" />
+            <SelectedDayStyle BackColor="#666666" Font-Bold="True" ForeColor="White" />
+            <SelectorStyle BackColor="#CCCCCC" />
+            <TitleStyle BackColor="#999999" BorderColor="Black" Font-Bold="True" />
+            <TodayDayStyle BackColor="#CCCCCC" ForeColor="Black" />
+            <WeekendDayStyle BackColor="#FFFFCC" />
+        </asp:Calendar>
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Label">用餐人數:</asp:Label>
+    
+        <asp:TextBox ID="textpeople" runat="server"  OnTextChanged="textpeople_TextChanged" onkeypress="return IsIntText();" TextMode="Number"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label6" runat="server" Text="Label">餐廳:</asp:Label>
+    
+        <asp:DropDownList ID="dwnres" runat="server" OnSelectedIndexChanged="dwnres_SelectedIndexChanged" AutoPostBack="True">
+            <asp:ListItem Value="=請選擇餐廳=">=請選擇餐廳=</asp:ListItem>
+            <asp:ListItem Value="美美早午餐">美美早午餐</asp:ListItem>
+            <asp:ListItem Value="大美樂比薩屋">大美樂比薩屋</asp:ListItem>
+            <asp:ListItem Value="鮮魚海鮮">鮮魚海鮮</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <asp:Label ID="Label7" runat="server" Text="Label">用餐時段:</asp:Label>
+    
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server" RepeatDirection="Horizontal" RepeatLayout="Flow" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged1">
+            <asp:ListItem Value="0"></asp:ListItem>
+            <asp:ListItem Value="1"></asp:ListItem>
+            <asp:ListItem Value="2"></asp:ListItem>
+            <asp:ListItem Value="3"></asp:ListItem>
+            <asp:ListItem Value="4"></asp:ListItem>
+            <asp:ListItem>5</asp:ListItem>
+        </asp:RadioButtonList>
+        <br />
+        <br />
+        <br />
+        <asp:Panel ID="Panel1" runat="server" Visible="False" TabIndex="2">
+            <asp:CheckBox ID="cbxhumbger" runat="server" Text="漢堡" />
+            <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                <asp:ListItem Value="1">1</asp:ListItem>
+                <asp:ListItem Value="2">2</asp:ListItem>
+                <asp:ListItem Value="3">3</asp:ListItem>
+                <asp:ListItem Value="4">4</asp:ListItem>
+                <asp:ListItem Value="5">5</asp:ListItem>
+            </asp:DropDownList>
+            份<br /> 
+            <asp:CheckBox ID="cbxsandwish" runat="server" Text="三明治" />
+            <asp:DropDownList ID="DropDownList5" runat="server" OnSelectedIndexChanged="DropDownList5_SelectedIndexChanged">
+                <asp:ListItem Value="1">1</asp:ListItem>
+                <asp:ListItem Value="2">2</asp:ListItem>
+                <asp:ListItem Value="3">3</asp:ListItem>
+                <asp:ListItem Value="4">4</asp:ListItem>
+                <asp:ListItem Value="5">5</asp:ListItem>
+            </asp:DropDownList>
+            份<br /> 
+            <asp:CheckBox ID="cbxpank" runat="server" Text="鬆餅" />
+            <asp:DropDownList ID="DropDownList6" runat="server" OnSelectedIndexChanged="DropDownList6_SelectedIndexChanged">
+                <asp:ListItem>1</asp:ListItem>
+                <asp:ListItem>2</asp:ListItem>
+                <asp:ListItem>3</asp:ListItem>
+                <asp:ListItem>4</asp:ListItem>
+                <asp:ListItem>5</asp:ListItem>
+            </asp:DropDownList>
+            份<br /> 
+            <asp:CheckBox ID="cbxsoup" runat="server" Text="玉米濃湯" />
+            <asp:DropDownList ID="DropDownList7" runat="server" OnSelectedIndexChanged="DropDownList7_SelectedIndexChanged">
+                <asp:ListItem Value="1">1</asp:ListItem>
+                <asp:ListItem Value="2">2</asp:ListItem>
+                <asp:ListItem Value="3">3</asp:ListItem>
+                <asp:ListItem Value="4">4</asp:ListItem>
+                <asp:ListItem Value="5">5</asp:ListItem>
+            </asp:DropDownList>
+            份</asp:Panel>
+        <br />
+        <br />
+    
+    </div>
+        <asp:Button ID="btn01" runat="server" Text="送出" OnClick="btn01_Click" />
+        <br />
+        <br />
+        <br />
+        <asp:Panel ID="Panel2" runat="server" Visible="False">
+            <asp:Label ID="textname1"  runat="server" ForeColor="Blue"></asp:Label>
+        <br />
+        
+        <asp:Label ID="texttel1"  runat="server" ForeColor="Blue"></asp:Label>
+        <br />
+        
+        <asp:Label ID="txtDate"  runat="server" ForeColor="Blue"></asp:Label>
+        <br />
+        <asp:Label ID="textpeople1"  runat="server" ForeColor="Blue"></asp:Label>
+        <br />
+        <asp:Label ID="txtres" runat="server" ForeColor="Blue"></asp:Label>
+        <br />
+        <asp:Label ID="txttime1" runat="server" ForeColor="Blue"></asp:Label>
+        <br />
+        <asp:Label ID="txtMsg" runat="server" ForeColor="Blue"></asp:Label>
+        </asp:Panel>
+        <br />
+        
+        
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void dwnres_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        Panel2.Visible = false;
+        switch (dwnres.SelectedValue)
+        {
+            case "=請選擇餐廳=":
+                RadioButtonList1.Visible = false;
+                Panel1.Visible = false;
+                textname1.Visible = false;
+                texttel1.Visible = false;
+                txtDate.Visible = false;
+                textpeople1.Visible = false;
+                txtres.Visible = false;
+                txttime1.Visible = false;
+                txtMsg.Visible = false;
+                break;
+
+            case "美美早午餐":
+                RadioButtonList1.Visible = true;
+                RadioButtonList1.Items[0].Text = "7:00";
+                RadioButtonList1.Items[1].Text = "8:00";
+                RadioButtonList1.Items[2].Text = "9:00";
+                RadioButtonList1.Items[3].Text = "10:00";
+                RadioButtonList1.Items[4].Text = "11:00";
+                RadioButtonList1.Items[5].Text = "12:00";
+
+                RadioButtonList1.Items[0].Selected = false;
+                RadioButtonList1.Items[1].Selected = false;
+                RadioButtonList1.Items[2].Selected = false;
+                RadioButtonList1.Items[3].Selected = false;
+                RadioButtonList1.Items[4].Selected = false;
+                RadioButtonList1.Items[5].Selected = false;
+                Panel1.Visible = true;
+                cbxhumbger.Checked = false;
+                cbxsandwish.Checked = false;
+                cbxpank.Checked = false;
+                cbxsoup.Checked = false;
+
+                cbxhumbger.Text = "漢堡";
+                cbxsandwish.Text = "三明治";
+                cbxpank.Text = "鬆餅";
+                cbxsoup.Text = "玉米濃湯";
+
+                for (int i = 1; i < 5; i++) {
+                    DropDownList1.Items[i].Selected = false;
+                    DropDownList5.Items[i].Selected = false;
+                    DropDownList6.Items[i].Selected = false;
+                    DropDownList7.Items[i].Selected = false;
+                }
+                DropDownList1.Items[0].Selected = true;
+                DropDownList5.Items[0].Selected = true;
+                DropDownList6.Items[0].Selected = true;
+                DropDownList7.Items[0].Selected = true;
+                    
+
+               
+                break;
+
+            case "大美樂比薩屋":
+
+                RadioButtonList1.Visible = true;
+                RadioButtonList1.Items[0].Text = "11:00";
+                RadioButtonList1.Items[1].Text = "12:00";
+                RadioButtonList1.Items[2].Text = "13:00";
+                RadioButtonList1.Items[3].Text = "17:00";
+                RadioButtonList1.Items[4].Text = "18:00";
+                RadioButtonList1.Items[5].Text = "19:00";
+
+                RadioButtonList1.Items[0].Selected = false;
+                RadioButtonList1.Items[1].Selected = false;
+                RadioButtonList1.Items[2].Selected = false;
+                RadioButtonList1.Items[3].Selected = false;
+                RadioButtonList1.Items[4].Selected = false;
+                RadioButtonList1.Items[5].Selected = false;
+                Panel1.Visible = true;
+                cbxhumbger.Checked = false;
+                cbxsandwish.Checked = false;
+                cbxpank.Checked = false;
+                cbxsoup.Checked = false;
+
+                cbxhumbger.Text = "夏威夷披薩";
+                cbxsandwish.Text = "海鮮披薩";
+                cbxpank.Text = "炸雞";
+                cbxsoup.Text = "可樂";
+                for (int i = 1; i < 5; i++)
+                {
+                    DropDownList1.Items[i].Selected = false;
+                    DropDownList5.Items[i].Selected = false;
+                    DropDownList6.Items[i].Selected = false;
+                    DropDownList7.Items[i].Selected = false;
+                }
+
+                DropDownList1.Items[0].Selected = true;
+                DropDownList5.Items[0].Selected = true;
+                DropDownList6.Items[0].Selected = true;
+                DropDownList7.Items[0].Selected = true;
+                
+                break;
+            case "鮮魚海鮮":
+
+                RadioButtonList1.Visible = true;
+                RadioButtonList1.Items[0].Text = "17:00";
+                RadioButtonList1.Items[1].Text = "18:00";
+                RadioButtonList1.Items[2].Text = "19:00";
+                RadioButtonList1.Items[3].Text = "20:00";
+                RadioButtonList1.Items[4].Text = "21:00";
+                RadioButtonList1.Items[5].Text = "22:00";
+
+                RadioButtonList1.Items[0].Selected = false;
+                RadioButtonList1.Items[1].Selected = false;
+                RadioButtonList1.Items[2].Selected = false;
+                RadioButtonList1.Items[3].Selected = false;
+                RadioButtonList1.Items[4].Selected = false;
+                RadioButtonList1.Items[5].Selected = false;
+                Panel1.Visible = true;
+                cbxhumbger.Checked = false;
+                cbxsandwish.Checked = false;
+                cbxpank.Checked = false;
+                cbxsoup.Checked = false;
+
+                cbxhumbger.Text = "焗烤龍蝦";
+                cbxsandwish.Text = "烤鯖魚";
+                cbxpank.Text = "炒三鮮";
+                cbxsoup.Text = "蛤蜊湯";
+
+                for (int i = 1; i < 5; i++)
+                {
+                    DropDownList1.Items[i].Selected = false;
+                    DropDownList5.Items[i].Selected = false;
+                    DropDownList6.Items[i].Selected = false;
+                    DropDownList7.Items[i].Selected = false;
+                }
+                DropDownList1.Items[0].Selected = true;
+                DropDownList5.Items[0].Selected = true;
+                DropDownList6.Items[0].Selected = true;
+                DropDownList7.Items[0].Selected = true;
+                break;
+
+        }
+    }
+    protected void textname_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void texttel_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void textpeople_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+
+
+    protected void btn01_Click(object sender, EventArgs e)
+    {
+        String tel = texttel.Text.ToString();
+        String date = Calendar1.SelectedDate.ToLongDateString();
+        int people = 0;
+        
+
+        if (textname.Text == "" || texttel.Text == "" || textpeople.Text == "")
+        {
+            Response.Write("<script>alert('不可為空白 請填寫資料')</script>");
+        }
+        else if (tel[0] != '0') { Response.Write("<script>alert('電話開頭必須是0')</script>"); }
+        else if (tel.Length != 10) { Response.Write("<script>alert('電話需是10碼')</script>"); }
+        else if (date == "0001年1月1日") { Response.Write("<script>alert('請選擇訂餐日期')</script>"); }
+        else if (dwnres.SelectedIndex == 0) { 
+            Response.Write("<script>alert('請選擇 用餐資訊')</script>");
+            Panel2.Visible = false;
+        }
+        else if (RadioButtonList1.SelectedIndex == -1) { Response.Write("<script>alert('請選擇用餐時段')</script>"); }
+        else
+        {
+            people = Int32.Parse(textpeople.Text.ToString());
+            if (people <= 0) { Response.Write("<script>alert('人數須為正整數')</script>"); }
+
+
+            Panel2.Visible = true;
+
+                if (cbxhumbger.Checked == false && cbxsandwish.Checked == false && cbxpank.Checked == false
+                        && cbxsoup.Checked == false) { Response.Write("<script>alert('請勾選餐點')</script>"); }
+                else
+                {
+                    txtMsg.Text = "";
+                    if (cbxhumbger.Checked)
+                    {
+                        txtMsg.Text += "你的選擇為：<br/>" + cbxhumbger.Text + DropDownList1.SelectedValue.ToString() + "份<br/>";
+                    }
+                    if (cbxsandwish.Checked)
+                    {
+                        txtMsg.Text += cbxsandwish.Text + DropDownList5.SelectedValue.ToString() + "份<br/>";
+
+                    }
+                    if (cbxpank.Checked)
+                    {
+                        txtMsg.Text += cbxpank.Text + DropDownList6.SelectedValue.ToString() + "份<br/>";
+
+                    }
+                    if (cbxsoup.Checked)
+                    {
+                        txtMsg.Text += cbxsoup.Text + DropDownList7.SelectedValue.ToString() + "份<br/>";
+
+                    }
+
+                    textname1.Text = "您的姓名:" + textname.Text.ToString();
+                    texttel1.Text = "您的電話:" + texttel.Text.ToString();
+                    txtDate.Text = "您的訂位日期:" + Calendar1.SelectedDate.ToLongDateString();
+                    textpeople1.Text = "您的訂位人數:" + people + "人";
+                    txtres.Text = "用餐餐廳:" + dwnres.SelectedValue.ToString();
+                    txttime1.Text = "您選擇的用餐時段為:" + RadioButtonList1.SelectedItem.ToString();
+                }
+
+            
+        }
+    }
+
+
+    protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+    {
+        if (e.Day.Date <= DateTime.Now.Date)
+        {
+            e.Day.IsSelectable = false;
+        }
+        else {
+            e.Day.IsSelectable = true;
+        }
+
+    }
+    protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        
+        
+    }
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DropDownList1.SelectedItem.Selected=true;
+    }
+    protected void DropDownList5_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DropDownList5.SelectedItem.Selected = true;
+    }
+    protected void DropDownList6_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DropDownList6.SelectedItem.Selected = true;
+    }
+    protected void DropDownList7_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DropDownList7.SelectedItem.Selected = true;
+    }
+    protected void RadioButtonList1_SelectedIndexChanged1(object sender, EventArgs e)
+    {
+
+    }
+}
